175b656c8893bd64c901d4b23c72bd3647f67c08
Pavel Prodan <prodanpavel05@gmail.com>
2022-08-24 18:04:09 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //home: HomePage(),
-      home: homeAPI(),
+      home: HomePage(),
     );
   }
 }
