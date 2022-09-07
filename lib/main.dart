9456472ac5cf5fdd6928b5efcb53968a20056061
Pavel Prodan <prodanpavel05@gmail.com>
2022-09-07 09:35:13 +0300

    cleanup home_api page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:textimo_mobile_app/views/check_connection.dart';
-import 'package:textimo_mobile_app/views/homeAPI.dart';
 import 'package:textimo_mobile_app/views/home_page.dart';
 import 'package:textimo_mobile_app/services/check_connection_service.dart';
 import 'package:textimo_mobile_app/views/connection_guide.dart';
