f7a3243b6be17d950ed79106aca08ea3791c42c4
Pavel Prodan <prodanpavel05@gmail.com>
2022-09-14 09:27:52 +0300

    update

---
+++ lib/views/projector.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProjectorWidget extends StatefulWidget {
+  const ProjectorWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ProjectorWidget> createState() => _ProjectorWidgetState();
+}
+
+class _ProjectorWidgetState extends State<ProjectorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
