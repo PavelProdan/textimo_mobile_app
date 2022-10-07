42774d73939df2e1e60067eaa3931144ee9e1614
Pavel Prodan <prodanpavel05@gmail.com>
2022-10-07 20:44:35 +0300

    .

--- lib/views/projector.dart
+++ lib/views/projector.dart
@@ -8,7 +8,6 @@ class ProjectorController extends GetxController {
   String? verseNumber = Get.arguments[1]['verse_number'];
 }
 
-
 class ProjectorWidget extends StatefulWidget {
   const ProjectorWidget({Key? key}) : super(key: key);
 
@@ -29,44 +28,56 @@ class _ProjectorWidgetState extends State<ProjectorWidget> {
         return true;
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("Proiectie Live"),
-          centerTitle: true,
-          backgroundColor: const Color(0xFF3F63F1),
-          actions: <Widget>[
-          IconButton(
-              icon: const Icon(Icons.hide_source),
-              tooltip: 'Opreste proiectia',
-              onPressed: () {} //showDialogTest(context),
+          appBar: AppBar(
+            title: Text("Proiectie Live"),
+            centerTitle: true,
+            backgroundColor: const Color(0xFF3F63F1),
+            actions: <Widget>[
+              IconButton(
+                  icon: const Icon(Icons.hide_source),
+                  tooltip: 'Opreste proiectia',
+                  onPressed: () {} //showDialogTest(context),
 
+                  ),
+            ],
+          ),
+          // body: Center(
+          //   child: Text("Now playing song with id: ${controller.songId} and verse number: ${controller.verseNumber}"),
+          // ),
+          // ignore: prefer_const_constructors
+
+          // add body that contains a centered title and a text aligned to the left
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text("now_playing_song_title",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 20)),
+                ),
               ),
-        ],
-
-        ),
-        // body: Center(
-        //   child: Text("Now playing song with id: ${controller.songId} and verse number: ${controller.verseNumber}"),
-        // ),
-        
-        // add body that contains a centered title and a text aligned to the left
-        body: Column(
-  children: <Widget>[
-    Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Center(
-        child: Text("now_playing_song_title",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.w800, fontSize: 20)),
-      ),
-    ),
-    SizedBox(height: 40),
-    
-Text("Strofa curenata: ${controller.verseNumber}/total",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.w800, fontSize: 15))    
-  ],
-)
-
-      ),
+              SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("Strofa curenta: ${controller.verseNumber}/total",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18)),
+              ),
+              Container(
+                color: Color.fromARGB(255, 219, 219, 219),
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("jhfjdfhjhfdjdhfjhdf \nyfdjyfjdy")),
+              )
+            ],
+          )),
     );
   }
 }
