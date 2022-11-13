29b5f95cbd86ca0b1dd8b25d92879449bc18e01e
Pavel Prodan <prodanpavel05@gmail.com>
2022-11-13 22:42:07 +0200

    basic example for search bar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -111,7 +111,12 @@ class _HomePageState extends State<HomePage> {
           IconButton(
               icon: const Icon(Icons.search),
               tooltip: 'Caută o melodie',
-              onPressed: () {} //showDialogTest(context),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: SearchSongDelegate(),
+                  );
+              } //showDialogTest(context),
 
               ),
         ],
@@ -285,4 +290,84 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+class SearchSongDelegate extends SearchDelegate {
+// Demo list to show querying
+List<String> searchTerms = [
+	"Apple",
+	"Banana",
+	"Mango",
+	"Pear",
+	"Watermelons",
+	"Blueberries",
+	"Pineapples",
+	"Strawberries"
+];
+	
+// first overwrite to
+// clear the search text
+@override
+List<Widget>? buildActions(BuildContext context) {
+	return [
+	IconButton(
+		onPressed: () {
+		query = '';
+		},
+		icon: Icon(Icons.clear),
+	),
+	];
+}
+
+// second overwrite to pop out of search menu
+@override
+Widget? buildLeading(BuildContext context) {
+	return IconButton(
+	onPressed: () {
+		close(context, null);
+	},
+	icon: Icon(Icons.arrow_back),
+	);
+}
+
+// third overwrite to show query result
+@override
+Widget buildResults(BuildContext context) {
+	List<String> matchQuery = [];
+	for (var fruit in searchTerms) {
+	if (fruit.toLowerCase().contains(query.toLowerCase())) {
+		matchQuery.add(fruit);
+	}
+	}
+	return ListView.builder(
+	itemCount: matchQuery.length,
+	itemBuilder: (context, index) {
+		var result = matchQuery[index];
+		return ListTile(
+		title: Text(result),
+		);
+	},
+	);
+}
+
+// last overwrite to show the
+// querying process at the runtime
+@override
+Widget buildSuggestions(BuildContext context) {
+	List<String> matchQuery = [];
+	for (var fruit in searchTerms) {
+	if (fruit.toLowerCase().contains(query.toLowerCase())) {
+		matchQuery.add(fruit);
+	}
+	}
+	return ListView.builder(
+	itemCount: matchQuery.length,
+	itemBuilder: (context, index) {
+		var result = matchQuery[index];
+		return ListTile(
+		title: Text(result),
+		);
+	},
+	);
+}
 }
